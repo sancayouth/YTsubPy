5ecc10233720e22c81ff1497f6c7f002d31c4b04
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-14 02:42:38 -0300

    Add main file

---
+++ main.py
@@ -0,0 +1,20 @@
+from app.Downloader import Downloader
+from app.SubMaker import SubMaker
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog='YTSubPy',
+        description='Small program to download subtitles from youtube')
+    parser.add_argument('url', help='the url of video')
+    r = parser.parse_args()
+    url = r.url
+    dwnld = Downloader.Downloader()
+    xml = dwnld.getxml(url)
+    sub = SubMaker.SubMaker()
+    sub.fromstring(xml)
+    sub.tofile()
+    print 'Subtitle file was generated successfully'
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,8 @@
+YTSubPy
+=================
+Small program to download subtitles from youtube (WIP)
+
+
+License
+------------
+Licensed under the MIT License.
