42f520c27d09838897d41dafd88e21a2d0ff8c66
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-14 02:42:01 -0300

    Add Downloader class

---
+++ app/Downloader/Downloader.py
@@ -0,0 +1,16 @@
+import urllib2
+from urlparse import urlparse, parse_qs
+
+class Downloader:
+
+    def getxml(self, url):
+        xml = ''
+        parsed_url = urlparse(url)
+        query = parse_qs(parsed_url.query)
+        if query.get('v')[0]:
+            url2 = 'http://www.youtube.com/api/timedtext?hl=en_US&v={video}'+\
+            '&type=track&lang=en&name=CC&kind&fmt=1'
+            url2 = url2.format(video = query.get('v')[0])
+            response = urllib2.urlopen(url2)
+            xml = response.read()
+        return xml
