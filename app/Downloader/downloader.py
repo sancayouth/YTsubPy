0ee23b4a61dacb0cd00e60f9d8389d700356da17
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-16 02:19:27 -0300

    Check if the url belongs to yt

--- app/Downloader/downloader.py
+++ app/Downloader/downloader.py
@@ -9,27 +9,29 @@ class Downloader:
         xml = ''
         parsed_url = urlparse(url)
         query = parse_qs(parsed_url.query)
-        v = query.get('v')[0]
-        if v:
-            url2 = 'http://www.youtube.com/api/timedtext?hl=en_US&v=' + \
-                    '{video}&type=track&lang=en&fmt=1'
-            code = self.get_config_xml(v)
-            if code == 'CC':
-                url2 = url2 + '&name=CC'
-            url2 = url2.format(video=v)
-            response = urlopen(url2)
-            xml = response.read()
+        if parsed_url.netloc == 'www.youtube.com':
+            v = query.get('v')[0]
+            if v:
+                url2 = 'https://www.youtube.com/api/timedtext?hl=en_US&v=' + \
+                        '{video}&type=track&lang=en&fmt=1'
+                code = self.get_config_xml(v)
+                if code == 'CC':
+                    url2 = url2 + '&name=CC'
+                url2 = url2.format(video=v)
+                response = urlopen(url2)
+                xml = response.read()
+            else:
+                raise NameError('Parameter v doesn\'t exists, check the url')
         else:
-            raise NameError('Parameter v doesn\'t exists, check the url')
+            raise NameError('The url is not valid')
         return xml
 
     def get_config_xml(self, video):
-        v = video
         code = ''
         try:
             url = 'https://www.youtube.com/api/timedtext?caps=asr&v={video}' +\
                 '&key=yttt1&hl=en_US&type=list&tlangs=1&fmts=0&vssids=1&asrs=1'
-            url = url.format(video=v)
+            url = url.format(video=video)
             response = urlopen(url)
             pxml = etree.fromstring(response.read())
             track = pxml.xpath('//track[@lang_code=\'en\']')
