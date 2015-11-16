2db87eb39e26fc494eef9b23e8088ae6be1f5af4
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-15 21:17:17 -0300

    Handle differents type of captions

--- app/Downloader/downloader.py
+++ app/Downloader/downloader.py
@@ -1,19 +1,39 @@
-import urllib2
+from  urllib2 import urlopen, HTTPError
 from urlparse import urlparse, parse_qs
+from lxml import etree
 
 
 class Downloader:
 
-    def getxml(self, url):
+    def get_xml(self, url):
         xml = ''
         parsed_url = urlparse(url)
         query = parse_qs(parsed_url.query)
-        if query.get('v'):
-            url2 = 'http://www.youtube.com/api/timedtext?hl=en_US&v={video}' +\
-            '&type=track&lang=en&name=CC&kind&fmt=1'
-            url2 = url2.format(video=query.get('v')[0])
-            response = urllib2.urlopen(url2)
+        v = query.get('v')[0]
+        if v:
+            url2 = 'http://www.youtube.com/api/timedtext?hl=en_US&v=' + \
+                    '{video}&type=track&lang=en&fmt=1'
+            code = self.get_config_xml(v)
+            if code == 'CC':
+                url2 = url2 + '&name=CC'
+            url2 = url2.format(video=v)
+            response = urlopen(url2)
             xml = response.read()
         else:
             raise NameError('Parameter v doesn\'t exists, check the url')
         return xml
+
+    def get_config_xml(self, video):
+        v = video
+        code = ''
+        try:
+            url = 'https://www.youtube.com/api/timedtext?caps=asr&v={video}' +\
+                '&key=yttt1&hl=en_US&type=list&tlangs=1&fmts=0&vssids=1&asrs=1'
+            url = url.format(video=v)
+            response = urlopen(url)
+            pxml = etree.fromstring(response.read())
+            track = pxml.xpath('//track[@lang_code=\'en\']')
+            code = track[0].attrib.get('name')
+        except HTTPError as e:
+            print e
+        return code
--- main.py
+++ main.py
@@ -8,16 +8,21 @@ def main():
     parser = argparse.ArgumentParser(prog='YTSubPy',
         description='Small program to download subtitles from youtube')
     parser.add_argument('url', help='the url of video')
+
     r = parser.parse_args()
     url = r.url
     try:
         dwnld = downloader.Downloader()
-        xml = dwnld.getxml(url)
-        sub = submaker.SubMaker()
-        sub.fromstring(xml)
-        sub.tofile()
-        print 'Subtitle was generated successfully'
+        xml = dwnld.get_xml(url)
+        if xml:
+            sub = submaker.SubMaker()
+            sub.fromstring(xml)
+            sub.tofile()
+            print 'Subtitle was generated successfully'
+        else:
+            raise NameError('Subtitle wasn\'t generated ')
     except HTTPError as e:
+        print e
         print 'Subtitle not Found'
     except IOError as e:
         print 'Subtitle wasn\'t generated : ' + e
