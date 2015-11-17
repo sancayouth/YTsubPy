48d6420936a529685bbf6e441be995292b6f6805
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-17 02:05:26 -0300

    Test if file is created

--- tests/test_submaker.py
+++ tests/test_submaker.py
@@ -1,8 +1,14 @@
 import unittest
+import os.path
 from app.SubMaker import submaker
 
 
 class Tests(unittest.TestCase):
+    def setUp(self):
+        self.xml = '''<?xml version="1.0" encoding="utf-8" ?>
+                 <transcript>
+                 <text start="8.58" dur="3.889">Hello</text>
+                 </transcript>'''
 
     def test_crated_empty_time(self):
         sub = submaker.SubMaker()
@@ -15,15 +21,17 @@ class Tests(unittest.TestCase):
                         sub.generate_time(8.58, 3.889))
 
     def test_generated_subs(self):
-        xml = '''<?xml version="1.0" encoding="utf-8" ?>
-                 <transcript>
-                 <text start="8.58" dur="3.889">Hello</text>
-                 </transcript>'''
         sub = submaker.SubMaker()
-        ret = sub.fromstring(xml)
+        ret = sub.fromstring(self.xml)
         sub_list = ['1\n', '00:00:08,580 --> 00:00:12,469\n', 'Hello\n', '\n']
         self.assertEqual(4, len(ret))
         self.assertEqual(sub_list, ret)
+
+    def test_srt_file_is_created(self):
+        sub = submaker.SubMaker()
+        ret = sub.fromstring(self.xml)
+        sub.tofile()
+        self.assertTrue(os.path.isfile('sub.srt'))
 
 
 if __name__ == '__main__':
