0cf01d3cede026502d240fee2e59ae710047c158
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-12-12 05:18:53 -0300

    Discover available Languages and download subtitles in diff. language

--- app/Downloader/downloader.py
+++ app/Downloader/downloader.py
@@ -5,37 +5,56 @@ from lxml import etree
 
 class Downloader:
 
-    def get_xml(self, url):
-        xml = ''
-        parsed_url = urlparse(url)
+    def __init__(self, url, lang='en'):
+        self.url = url
+        self.v = ''
+        self.lang = lang
+        self.parse()
+
+    def parse(self):
+        parsed_url = urlparse(self.url)
         query = parse_qs(parsed_url.query)
-        if parsed_url.netloc == 'www.youtube.com':
-            v = query.get('v')[0]
-            if v:
-                url2 = 'https://www.youtube.com/api/timedtext?hl=en_US&v=' + \
-                        '{video}&type=track&lang=en&fmt=1'
-                code = self.get_config_xml(v)
-                if code == 'CC':
+        error = NameError('The url is not valid')
+        if parsed_url.netloc == 'www.youtube.com' and query.get('v'):
+            self.v = query.get('v')[0]
+            if not self.v:
+                raise error
+        else:
+            raise error
+
+    def get_xml(self):
+        xml = ''
+        try:
+            url2 = 'https://www.youtube.com/api/timedtext?hl=en_US&v=' + \
+                    '{video}&type=track&lang={lang}&fmt=1'
+            conf = self.get_config_xml()
+            if len(conf):
+                if conf['CC']:
                     url2 = url2 + '&name=CC'
-                url2 = url2.format(video=v)
+                url2 = url2.format(video=self.v, lang=self.lang)
                 response = urlopen(url2)
                 xml = response.read()
-            else:
-                raise NameError('Parameter v doesn\'t exists, check the url')
-        else:
-            raise NameError('The url is not valid')
+        except HTTPError as e:
+            print e
         return xml
 
-    def get_config_xml(self, video):
-        code = ''
+    def get_config_xml(self, discover=False):
+        code = {}
         try:
             url = 'https://www.youtube.com/api/timedtext?caps=asr&v={video}' +\
                 '&key=yttt1&hl=en_US&type=list&tlangs=1&fmts=0&vssids=1&asrs=1'
-            url = url.format(video=video)
+            url = url.format(video=self.v)
             response = urlopen(url)
             pxml = etree.fromstring(response.read())
-            track = pxml.xpath('//track[@lang_code=\'en\']')
-            code = track[0].attrib.get('name')
+            if discover:
+                tracks = pxml.xpath('//track')
+                for track in tracks:
+                    print  '%r:%r' % \
+                    (track.get('lang_translated'), track.get('lang_code'))
+            else:
+                track = pxml.xpath('//track[@lang_code=%r]' % self.lang)
+                if track:
+                    code['CC'] = track[0].attrib.get('name')
         except HTTPError as e:
             print e
         return code
--- main.py
+++ main.py
@@ -8,19 +8,27 @@ def main():
     parser = argparse.ArgumentParser(prog='YTSubPy',
         description='Small program to download subtitles from youtube')
     parser.add_argument('url', help='the url of video')
+    parser.add_argument('-l', help='language', default='en')
+    parser.add_argument('-d', help='discover the available languages',
+                        action='store_true')
 
     r = parser.parse_args()
     url = r.url
+    lang = r.l
+    discover = r.d
     try:
-        dwnld = downloader.Downloader()
-        xml = dwnld.get_xml(url)
-        if xml:
-            sub = submaker.SubMaker()
-            sub.fromstring(xml)
-            sub.tofile()
-            print 'Subtitle was generated successfully'
+        dwnld = downloader.Downloader(url, lang)
+        if not discover:
+            xml = dwnld.get_xml()
+            if xml:
+                sub = submaker.SubMaker()
+                sub.fromstring(xml)
+                sub.tofile()
+                print 'Subtitle was generated successfully'
+            else:
+                raise NameError('Subtitle wasn\'t generated ')
         else:
-            raise NameError('Subtitle wasn\'t generated ')
+            dwnld.get_config_xml(discover)
     except HTTPError as e:
         print e
         print 'Subtitle not Found'
@@ -28,7 +36,6 @@ def main():
         print 'Subtitle wasn\'t generated : ' + e
     except Exception as e:
         print e
-
 
 if __name__ == '__main__':
     main()
